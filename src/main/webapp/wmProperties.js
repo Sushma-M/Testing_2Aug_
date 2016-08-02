6de8207845094d67f40d2f3277f8f1888895b8b5
root <root@50d297549a19>
2016-08-02 09:22:13 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Testing26thJuly_",
+  "homePage" : "Main",
+  "name" : "Testing26thJuly_",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
---
+++ src/test/resources/README.md
@@ -0,0 +1,2 @@
+Use this folder to add your test resources such as properties and xml files.
+
